f04461e66f3b31794be919c3b658268f7bc96cdb
Student <Student@1912832-C20232.lanecc.edu>
2018-05-24 10:12:53 -0700

    Wrote Constructor added css and changed html title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state= 
+    {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      winner: null,
+      winningLine: Array(),
+    }
+   
+    this.lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        ];
+
+  }
+
+
+
   render() {
     return (
       <div className="App">
